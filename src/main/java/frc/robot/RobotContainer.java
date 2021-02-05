9f8e5e9566a7335cae4c2f8af57937b1e1ac198d
alya222 <alya_nimis-ibrahim@isd31.net>
2021-02-05 15:36:11 -0600

    test commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,81 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Axis;
+import edu.wpi.first.wpilibj.XboxController.Button;
+
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Flag;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+
+/**
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  private final XboxController xbox = new XboxController(Constants.kControllerPort);
+  // The robot's subsystems and commands are defined here...
+  private final Drivetrain drivetrain = new Drivetrain();
+  
+  private final Flag flag = new Flag();
+
+  private Command manualDrive = new RunCommand( 
+    () -> drivetrain.getDifferentialDrive().tankDrive(
+      xbox.getRawAxis(Axis.kLeftY.value),
+      xbox.getRawAxis(Axis.kLeftY.value),
+      false
+    ), 
+    drivetrain
+    
+    );
+
+
+
+  /**
+   * The container for the robot.  Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+
+    drivetrain.setDefaultCommand(manualDrive);
+  }
+
+  /**
+   * Use this method to define your button->command mappings.  Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
+   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    new JoystickButton(xbox, Button.kY.value)
+    .whenPressed(new InstantCommand(() -> flag.flagSwitch(), flag));
+  }
+
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return null;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.*;
+
+public class Drivetrain extends SubsystemBase { 
+    private CANSparkMax LFrontWheel = new CANSparkMax(kLeftFrontPort, MotorType.kBrushless);
+    private CANSparkMax RFrontWheel = new CANSparkMax(kRightFrontPort, MotorType.kBrushless);
+    
+    private CANSparkMax LRearWheel = new CANSparkMax(kLeftRearPort, MotorType.kBrushless);
+    private CANSparkMax RRearWheel = new CANSparkMax(kRightRearPort, MotorType.kBrushless);
+
+    private DifferentialDrive roboDrive = new DifferentialDrive(LFrontWheel, RFrontWheel); 
+
+  public Drivetrain() { 
+      LRearWheel.follow(LFrontWheel); 
+      RRearWheel.follow(RFrontWheel);
+
+  }
+
+  public DifferentialDrive getDifferentialDrive () { 
+      return roboDrive;
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
